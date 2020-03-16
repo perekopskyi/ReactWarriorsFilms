6af8aa9d4c26ec916c1db548824feb1d19366271
i_am_yevhen <yevheniiperekopskyi@gmail.com>
2020-03-16 15:51:27 +0300

    Updates

---
+++ precache-manifest.6dd038da8060cf4916c5439000cb2fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "112ddf024333855ada18",
+    "url": "/ReactWarriors-Films/static/js/main.f9edccde.chunk.js"
+  },
+  {
+    "revision": "5fc5e3e99293e94a5465",
+    "url": "/ReactWarriors-Films/static/js/runtime~main.09fd7c2b.js"
+  },
+  {
+    "revision": "6ece50af01927d4c0056",
+    "url": "/ReactWarriors-Films/static/css/2.0bcc2361.chunk.css"
+  },
+  {
+    "revision": "6ece50af01927d4c0056",
+    "url": "/ReactWarriors-Films/static/js/2.953971d9.chunk.js"
+  },
+  {
+    "revision": "c07009cfd573fa8538461c7217b3a404",
+    "url": "/ReactWarriors-Films/static/media/unnamed.c07009cf.jpg"
+  },
+  {
+    "revision": "2f1e8f264e1ec8affbad752040ad7026",
+    "url": "/ReactWarriors-Films/index.html"
+  }
+];
